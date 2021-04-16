13b0af42d1edfdf00432c41a10cc25e2428de42f
Emmanuel <emmanuelamon92@gmail.com>
2021-04-16 14:33:13 -0400

    Complete assignment

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
+
 function addingEventListener() {
+    const input = document.getElementById('input');
+    alert('I was clicked!');
+    input.addEventListener('click', addingEventListener); 
 }
+
